3de3f31d5dc0dab8b33f7a6300f2bde0457bb185
samw90 <samwilder90@gmail.com>
2026-06-19 17:19:38 -0500

    update

---
+++ math-quest-pwa/sw.js
@@ -0,0 +1,27 @@
+// Bump CACHE (v1 -> v2 ...) whenever you upload a new index.html so devices refresh.
+const CACHE = 'mathquest-v4';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon-32.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) =>
+      hit || fetch(e.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return resp;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
